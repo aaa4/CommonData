264362f43ef58c9594e6431265101f1b715c49d2
Alex <KavNec@gmail.com>
2020-09-25 16:21:10 +0300

    Initial commit

---
+++ src/main/java/com/github/aaa4/CommonSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.github.aaa4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonSpringDataApplication.class, args);
+    }
+
+}
